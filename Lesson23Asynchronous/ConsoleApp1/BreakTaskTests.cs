15eb3543584c06c27b606fc7ec87ea85c9594943
Danylo Raduchych <danil.r87602@gmail.com>
2023-06-22 19:52:42 +0200

    1212

---
+++ Lesson23Asynchronous/ConsoleApp1/BreakTaskTests.cs
@@ -0,0 +1,75 @@
+﻿
+
+
+
+var heatThePen = Task.Run(HeatUpAPen);
+var boilWater = Task.Run(BoilSomeWater);
+
+
+
+
+
+await Task.WhenAll(heatThePen);
+var fryToEggs = Task.Run(FryTwoEEggs);
+await fryToEggs;
+var fryThreeSlicesOfBacon = Task.Run(() => FryThreeSlicesOfBacon());
+
+await Task.WhenAll(boilWater);
+var coffe = Task.Run(MakeSomeCoffe);
+
+await Task.WhenAll(fryThreeSlicesOfBacon, coffe);
+
+
+
+while (true)
+{
+
+    await Task.Delay(1000);
+
+}
+
+
+
+Console.ReadLine();
+
+
+async void HeatUpAPen()
+{
+
+    Thread.Sleep(1000);
+    Console.WriteLine("i'm firing pen...");
+}
+
+async void BoilSomeWater()
+{
+
+    Thread.Sleep(5000);
+    Console.WriteLine("i'm firing water...");
+}
+
+async void FryTwoEEggs()
+{
+
+    Thread.Sleep(3000);
+    Console.WriteLine("i'm firing eggs...");
+}
+
+async void FryThreeSlicesOfBacon()
+{
+
+    Thread.Sleep(2000);
+    Console.WriteLine("i'm firing bacon...");
+}
+
+
+async void MakeSomeCoffe()
+{
+
+    Thread.Sleep(1000);
+    Console.WriteLine("i'm firing coffee...");
+}
+
+
+
+
+
---
+++ Lesson23Asynchronous/Lesson23Asynchronous/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+
+using System.Net;
+
+List<Task<string>> downloadTasks = new List<Task<string>>();
+
+
+var googleTaks = Task.Run(() => DownloadUrl("https://google.com"));
+
+var duckTask = Task.Run(() => DownloadUrl("https://duckducgo.com"));
+
+var yahooTak = Task.Run(() => DownloadUrl("https://www.yahoo.com"));
+
+var resultArray = await Task.WhenAll(googleTaks, duckTask, yahooTak);
+bool allAreUp = true;
+foreach (var item in resultArray)
+{
+    if (item.StatusCode != HttpStatusCode.OK)
+    {
+        allAreUp = false; 
+    }
+}
+
+Console.WriteLine(allAreUp ? "All sites are up" : "Not every site is  running");
+await Task.Delay(2000);
+
+async Task<HttpResponseMessage> DownloadUrl(string url)
+{
+    using (var httpClient = new HttpClient())
+    {
+        try
+        {
+return  await httpClient.GetAsync(url);
+        }
+        catch (Exception e)
+        {
+            return new HttpResponseMessage(HttpStatusCode.NotFound);
+        }
+    
+        
+    
+    }
+}
+
+
+Console.ReadLine();
